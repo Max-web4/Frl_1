cb76845758b39ff07278b41f2689b1971e4f687a
Max <yudinmax4444@gmail.com>
2020-12-09 19:08:06 +0200

    init changes

--- js/maim.js
+++ js/maim.js
@@ -1,8 +1,21 @@
-for (let i = 0; i <= 5; i++) {
-    console.log(i)
+"use strict"
+// Задание  1
+// Петя первый
+
+    function showName() {
+console.log("Петя!")
+    }
+setTimeout(showName, 0)
+
+console.log("Петя!")
+   
+// Задание 2 - Верно
+// Задание 3 - Не верно
+
+//Задание 4
+let  showText ;
+if(2>1){
+    showText = function () {console.log("you are the best programmer!")
+    }
 }
-let i = 0 ;
-while (i <= 5) {
-    
-    console.log(i++)
-}
+showText()
